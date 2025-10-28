c941e2e5ffa5e6690e0c20039b040b961181d6a9
arafat-zihad <arafat.zihad01@gmail.com>
2025-10-28 21:27:44 +0600

    variables

---
+++ Fundamentals/VariablesDatatype/Variable_DataType.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring and initializing a variable
+	var a int = 10
+	fmt.Println("Value of a:", a)
+
+	// Let's demonstrate more variable concepts
+	var b = 20 // Type inference
+	c := 30    // Short declaration
+
+	fmt.Println("Value of b:", b)
+	fmt.Println("Value of c:", c)
+}
