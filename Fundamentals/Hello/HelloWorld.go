f4d113e7765141a5361977f2fece7b60393aa1be
arafat-zihad <arafat.zihad01@gmail.com>
2025-11-07 18:38:35 +0600

    variable

--- Fundamentals/Hello/HelloWorld.go
+++ Fundamentals/Hello/HelloWorld.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+
+	// Variable Declaration with inicial Value
+	var std1 string = "Jhon"
+	var std2 = "Zihad"
+	x := 2
+
+	fmt.Println(std1)
+	fmt.Println(std2)
+	fmt.Println(x)
+
+	// Variable Declaration without inicial Value
+	var a string
+	var b int
+	var c bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
 }
--- Fundamentals/VariablesDatatype/Variable_DataType.go
+++ Fundamentals/VariablesDatatype/Variable_DataType.go
@@ -1,16 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// Declaring and initializing a variable
-	var a int = 10
-	fmt.Println("Value of a:", a)
 
-	// Let's demonstrate more variable concepts
-	var b = 20 // Type inference
-	c := 30    // Short declaration
+	// Variable Declaration with inicial Value
+	var std1 string = "Jhon"
+	var std2 = "Zihad"
+	x := 2
 
-	fmt.Println("Value of b:", b)
-	fmt.Println("Value of c:", c)
+	fmt.Println(std1)
+	fmt.Println(std2)
+	fmt.Println(x)
+
+	// Variable Declaration without inicial Value
+	var a string
+	var b int
+	var c bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	// Multiple Variable Declaration
+	var a1, b1 = 6, "Hello"
+	c1, d1 := 7, "World"
+
+	fmt.Println(a1)
+	fmt.Println(b1)
+	fmt.Println(c1)
+	fmt.Println(d1)
+
+	// Variable Declaraton in block
+	var (
+		a2 int
+		b2 int    = 1
+		c2 string = "Hola"
+	)
+
+	fmt.Println(a2)
+	fmt.Println(b2)
+	fmt.Println(c2)
+
 }
